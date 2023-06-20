f33b39e633079a2bcdf51d2a8fe316df3e3a88d0
Saad Tajwar <saads@ad.unc.edu>
2023-06-19 23:41:52 -0400

    Completed 4

---
+++ controllers/blogs.js
@@ -0,0 +1,70 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
+    response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+    const {title, author, url, likes, userId} = request.body
+    if (!title || !url) {
+        return response.status(400).end()
+    }
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({error: 'token invalid'})
+    }
+
+    const user = await User.findById(decodedToken.id)
+    const newBlog = {
+        title,
+        author,
+        url,
+        likes: likes || 0,
+        user: userId
+    }
+
+    const blog = new Blog(newBlog)
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog)
+    await user.save()
+    response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    try {
+        await Blog.findByIdAndDelete(request.params.id)
+        response.status(204).end()
+    } catch(exception) {
+        console.log(exception);
+    }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const newBlog = {
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        likes: request.body.likes
+    }
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlog, {new: true})
+        response.json(updatedBlog)
+    } catch(exception) {
+        console.log(exception);
+    }
+})
+
+module.exports = blogsRouter
